3636a7ad5a6de6bae160cd16f563bdafd07fa23e
Rustam1827 <33642724+Rustam1827@users.noreply.github.com>
2021-05-05 21:11:32 -0400

    Update HelloWorld.java
    True bill

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -3,7 +3,7 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("I luv bubu!");
         System.out.println("I luv panda!");
-        System.out.println("I luv Belkas");
+        System.out.println("I dont luv Belka");
         System.out.println("I luv Nyahska");
         System.out.println("I feel your lovka");
         System.out.println("Too much luv I believe");
