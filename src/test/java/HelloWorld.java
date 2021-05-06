5c66dc9dd598860e8f7e3792a6e3c6e16541a6a7
rustam1827 <fidelity1827@gmail.com>
2021-05-05 20:47:35 -0400

    new luv

--- src/test/java/HelloWorld.java
+++ src/test/java/HelloWorld.java
@@ -4,5 +4,7 @@ public class HelloWorld {
         System.out.println("I luv bubu!");
         System.out.println("I luv panda!");
         System.out.println("I luv Belkas");
+        System.out.println("I luv Nyahska");
+
     }
 }
